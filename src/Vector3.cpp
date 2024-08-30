9e910345c4db6e35fa4bae80d3b022457f6af6ef
ORDRONNEAU Camille <Camille.Ordronneau@bordeaux-inp.fr>
2024-08-29 20:04:01 -0400

    Remove intermediate ret value and fix normalize when norm is null

--- src/Vector3.cpp
+++ src/Vector3.cpp
@@ -4,8 +4,7 @@
 Vector3::Vector3(const float x, const float y, const float z) : x(x), y(y), z(z) {}
 
 Vector3 Vector3::operator*(const float alpha) const {
-    Vector3 ret(alpha * this->x, alpha * this->y, alpha * this->z);
-    return ret;
+    return Vector3(alpha * this->x, alpha * this->y, alpha * this->z);
 }
 
 Vector3& Vector3::operator*=(const float alpha) {
@@ -16,13 +15,11 @@ Vector3& Vector3::operator*=(const float alpha) {
 }
 
 Vector3 operator*(const float alpha, const Vector3& vector) {
-    Vector3 ret(alpha * vector.x, alpha * vector.y, alpha * vector.z);
-    return ret;
+    return Vector3(alpha * vector.x, alpha * vector.y, alpha * vector.z);
 }
 
 Vector3 Vector3::operator+(const Vector3& vector) const {
-    Vector3 ret(this->x + vector.x, this->y + vector.y, this->z + vector.z);
-    return ret;
+    return Vector3(this->x + vector.x, this->y + vector.y, this->z + vector.z);
 }
 
 Vector3& Vector3::operator+=(const Vector3& vector) {
@@ -42,14 +39,18 @@ float Vector3::norm(const Vector3& vector) {
 
 Vector3 Vector3::normalize() const {
     float norm = this->norm();
-    Vector3 ret(this->x / norm, this->y / norm, this->z / norm);
-    return ret;
+    if (norm > 0) {
+        return Vector3(this->x / norm, this->y / norm, this->z / norm);
+    }
+    return Vector3(1, 0, 0);
 }
 
 Vector3 Vector3::normalize(const Vector3& vector) {
     float norm = vector.norm();
-    Vector3 ret(vector.x / norm, vector.y / norm, vector.z / norm);
-    return ret;
+    if (norm > 0) {
+        return Vector3(vector.x / norm, vector.y / norm, vector.z / norm);
+    }
+    return Vector3(1, 0, 0);
 }
 
 float Vector3::dot(const Vector3& vector_u, const Vector3& vector_v) {
@@ -57,8 +58,7 @@ float Vector3::dot(const Vector3& vector_u, const Vector3& vector_v) {
 }
 
 Vector3 Vector3::cross(const Vector3& vector_u, const Vector3& vector_v) {
-    Vector3 ret(vector_u.y * vector_v.z - vector_u.z * vector_v.y,
+    return Vector3(vector_u.y * vector_v.z - vector_u.z * vector_v.y,
         vector_u.z * vector_v.x - vector_u.x * vector_v.z,
         vector_u.x * vector_v.y - vector_u.y * vector_v.x);
-    return ret;
 }
